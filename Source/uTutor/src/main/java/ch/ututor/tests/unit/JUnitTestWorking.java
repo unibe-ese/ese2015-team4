9a68f6135054e6124b01c4ce9bb4c582b424325a
Mathias Stocker <mathias.stocker@students.unibe.ch>
2015-11-06 09:55:02 +0100

    test setup and first unit test
    commit 877fd59c652b022f6bb2d41a440e2f7cde63fe89
    Author: Mathias Stocker <mathias.stocker@students.unibe.ch>
    Date:   Thu Nov 5 16:27:47 2015 +0100
    
        Problem with SpringTestNotWorking
    
        getting initializationError

---
+++ Source/uTutor/src/main/java/ch/ututor/tests/unit/JUnitTestWorking.java
@@ -0,0 +1,13 @@
+package ch.ututor.tests.unit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class JUnitTestWorking {
+	
+	@Test
+	public void testTest(){
+		assertTrue(true);
+	}
+}
---
+++ Source/uTutor/src/main/java/ch/ututor/tests/unit/SingnupServiceTest.java
@@ -0,0 +1,63 @@
+package ch.ututor.tests.unit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import ch.ututor.controller.exceptions.form.PasswordRepetitionException;
+import ch.ututor.controller.exceptions.form.UserAlreadyExistsException;
+import ch.ututor.controller.pojos.SignUpForm;
+import ch.ututor.controller.service.SignupService;
+import ch.ututor.model.User;
+import ch.ututor.model.dao.UserDao;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.AdditionalAnswers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/test.xml"})
+public class SingnupServiceTest {
+	
+	@Autowired SignupService signupService;
+	@Autowired UserDao userDao;
+	private SignUpForm signUpForm;
+	
+	@Before
+	public void setupSignup(){
+		signUpForm = new SignUpForm();
+		signUpForm.setFirstName("Harry");
+		signUpForm.setLastName("Potter");
+		signUpForm.setEmail("harry.potter@hogwarts.com");
+		signUpForm.setPassword("12345678");
+		signUpForm.setPasswordRepeat("12345678");
+		when(userDao.save(any(User.class))).then(returnsFirstArg());
+		when(userDao.findByUsername(any(String.class))).thenReturn(null);
+	}
+	
+	@Test
+	public void testCreateUserAccountSuccessfull(){
+		
+		User user = signupService.createUserAccount(signUpForm);
+		
+		assertEquals("Harry", user.getFirstName());
+		assertEquals("Potter", user.getLastName());
+		assertEquals("harry.potter@hogwarts.com", user.getUsername());
+	}
+	
+	@Test(expected = PasswordRepetitionException.class)
+	public void testPasswordRepetitionException(){
+		signUpForm.setPasswordRepeat("Irgendwas");
+		signupService.createUserAccount(signUpForm);
+	}
+	
+	@Test(expected = UserAlreadyExistsException.class)
+	public void testUserAlreadyExistsException(){
+		when(userDao.findByUsername(any(String.class))).thenReturn(new User());
+		signupService.createUserAccount(signUpForm);
+	}
+}
